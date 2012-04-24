edfb731c0544afd5e0ae63d9784390b2205c1bbc
Alec Hipshear <alechipshear@gmail.com>
2012-04-24 14:29:33 -0400

    Spec states that either name is fine
    According to the spec[1], either short_name or long_name are required,
    but having at least one is sufficient.
    
    [1] https://developers.google.com/transit/gtfs/reference#routes_fields

--- lib/gtfs/route.rb
+++ lib/gtfs/route.rb
@@ -2,11 +2,16 @@ module GTFS
   class Route
     include GTFS::Model
 
-    has_required_attrs :id, :short_name, :long_name, :type
-    has_optional_attrs :agency_id, :desc, :url, :color, :text_color
+    has_required_attrs :id, :either_name, :type
+    has_optional_attrs :agency_id, :desc, :url, :color, :text_color, :short_name, :long_name
     attr_accessor *attrs
 
     column_prefix :route_
+
+    # Prefer the short name, but either is acceptable according to spec
+    def either_name
+      return @short_name || @long_name
+    end
 
     def self.parse_routes(data)
       return parse_models(data)
